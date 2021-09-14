fd44dc4cb132a9ab541133c672929f2dc6371815
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-09-14 16:42:15 +0900

    Fix

--- frontend/src/Pages/View.jsx
+++ frontend/src/Pages/View.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, useParams } from "react-router-dom";
+import { BrowserRouter as useParams } from "react-router-dom";
 import {
   Center,
   Box,
